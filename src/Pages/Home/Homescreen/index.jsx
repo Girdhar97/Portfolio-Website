89a131180dc56c9524e9fd5de2f0581c3b3789a6
Girdhar Saini <girdharscs@gmail.com>
2026-02-20 19:50:00 +0200

    Tweak homescreen layout composition

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -4,7 +4,7 @@ import Footer from "../Footer";
 import HeroSection from "../HeroSection";
 import MyPortfolio from "../MyPortfolio";
 import MySkills from "../MySkills";
-import Testimonial from "../Testimonials";
+// import Testimonial from "../Testimonials";
 import Education from "../Education";           
 import WorkExperience from "../WorkExperience"; 
 
@@ -12,12 +12,18 @@ export default function Home() {
   return (
     <>
       <HeroSection />
+      <div className="section-divider anim" />
       <MySkills />
+      <div className="section-divider anim" />
       <AboutMe />
+      <div className="section-divider anim" />
       <Education />
+      <div className="section-divider anim" />
       <WorkExperience />
+      <div className="section-divider anim" />
       <MyPortfolio />
-      <Testimonial />
+      {/* <div className="section-divider anim" /> */}
+      {/* <Testimonial /> */}
       <ContactMe />
       <Footer />
     </>
